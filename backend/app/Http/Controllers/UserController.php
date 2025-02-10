8514d6fb5fe7d0b97a8e11fecfaf1dca3502814d
Oier <oier.albeniz@ikasle.egibide.org>
2025-02-10 13:54:49 +0100

    Implementacion de metodos en los controladores ActividadController y UserController

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'required|string|max:255',
+            'apellido1' => 'required|string|max:255',
+            'apellido2' => 'required|string|max:255',
+            'dni' => 'required|string|max:255|unique:users',
+            'fecha_nacimiento' => 'required|date',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $usuario = User::create([
+            'nombre'=> $request->get('nombre'),
+            'apellido1'=> $request->get('apellido1'),
+            'apellido2'=> $request->get('apellido2'),
+            'dni'=> $request->get('dni'),
+            'fecha_nacimiento'=> $request->get('fecha_nacimiento'),
+        ]);
+
+        return response()->json(['message' => 'Usuario creado','data'=>$usuario], 201);
+    }
+
+    public function show($id) {
+        $usuario = User::findOrFail($id);
+        return response()->json(['message' => 'Usuario encontrado','data'=>$usuario], 201);
+    }
+
+    public function index() {
+        $usuarios = User::all();
+        return response()->json(['message' => 'Usuarios encontrados','data'=>$usuarios], 201);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ActividadController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('actividad', [ActividadController::class, 'store']);
+Route::controller(ActividadController::class)->group(function () {
+    Route::post('/actividades/store', 'store');
+    Route::post('/actividades/update/{id}', 'update');
+    Route::post('/actividades/destroy/{id}', 'destroy');
+    Route::get('/actividades/show/{id}', 'show');
+    Route::get('/actividades/index', 'index');
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::post('/usuarios/store', 'store');
+    Route::get('/usuarios/index', 'index');
+    Route::get('/usuarios/show/{id}', 'show');
+    Route::get('/usuarios/index', 'index');
+});
