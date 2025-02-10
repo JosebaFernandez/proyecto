1dfc071d0b7369a34abd3ac44f37b55780ca9967
unknown <1gdaw01@ARRIAGAINFO.LOCAL>
2025-02-10 10:47:13 +0100

    Modelo en backend

--- backend/app/Models/Admin.php
+++ backend/app/Models/Admin.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
-    //
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'nombre',
+        'apellido1',
+        'apellido2',
+        'dni',
+        'fecha_nacimiento',
+        'email',
+        'password',
+    ];
 }
