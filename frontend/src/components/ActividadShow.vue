a219bdc3add30d42936039fee435086bc10c66f5
unknown <1gdaw01@ARRIAGAINFO.LOCAL>
2025-02-17 11:45:58 +0100

    Ptolomeo

--- frontend/src/components/ActividadShow.vue
+++ frontend/src/components/ActividadShow.vue
@@ -16,7 +16,7 @@
                   @click="apuntarse(actividad.id)">
                   Apuntarse
                 </button>
-                <button v-if="rol == 'admin'" class="btn btn-outline-warning mr-2" type="button">
+                <button v-if="rol == 'admin'" class="btn btn-outline-warning mr-2" type="button" @click="goToUpdate()">
                   <svg xmlns="http://www.w3.org/2000/svg" width="21" height="25" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                   <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                   </svg>  
@@ -136,7 +136,8 @@
           }
         } catch (error) {
           console.error('Error al obtener la actividad:', error);
-
+        }
+      },
     goToUpdate() {
       this.$router.push(`/actividades/${this.actividad.id}/editar`);
     },
@@ -148,7 +149,10 @@
           console.error("Error: idActividad o userId son undefined.");
           return;
         }
-      },
+      } catch (error) {
+        console.error("Error al inscribirse:", error.response?.data || error);
+      }
+    },
       formatHora(hora) {
       if (!hora) return ""; // Si no hay hora, devolver vacío
       return hora.slice(0, 5); // Toma solo "HH:mm"
